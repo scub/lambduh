24973601e259cfc232d3545a8852729d30011081
Scub <scub@darchoods.net>
2019-08-24 12:04:47 -0400

    Strike image removal as final stage

--- sbin/build.sh
+++ sbin/build.sh
@@ -3,4 +3,4 @@
 build_name="lamduh-${RANDOM}"
 docker build -t ${build_name} .
 docker run --rm --mount type=bind,source="$(pwd)"/build,target=/build -it ${build_name}
-docker rmi -f ${build_name}
+#docker rmi ${build_name}
